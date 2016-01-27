48f3f417103b46841837d8a8231388f9cc991376
tiretfa <tiretfabien@hotmail.fr>
2016-01-27 14:27:57 +0100

    update

--- Interface/dialogseamcarving.cpp
+++ Interface/dialogseamcarving.cpp
@@ -1,5 +1,40 @@
 #include "dialogseamcarving.h"
 
-dialogseamCarving::dialogseamCarving()
+DialogSeamCarving::DialogSeamCarving()
 {
+
+    largeur = new QSpinBox(this);
+    hauteur = new QSpinBox(this);
+    largeur->setMinimum(0);
+    largeur->setMaximum(1500);
+    hauteur->setMinimum(0);
+    hauteur->setMaximum(1500);
+    QPushButton *bouton1 = new QPushButton("Ok");
+    QPushButton *bouton2 = new QPushButton("Annuler");
+    QHBoxLayout *layout2 = new QHBoxLayout;
+    layout2->addWidget(bouton1);
+    layout2->addWidget(bouton2);
+
+    QFormLayout *layout = new QFormLayout;
+    layout->addRow("Nombre de lignes de pixels à supprimer en largeur  : ", largeur);
+    layout->addRow("Nombre de lignes de pixels à supprimer en hauteur : ", hauteur);
+
+    QVBoxLayout *layoutPrincipal = new QVBoxLayout;
+    layoutPrincipal->addLayout(layout);
+    layoutPrincipal->addLayout(layout2);
+
+    this->setLayout(layoutPrincipal);
+
+    connect(bouton1,SIGNAL(clicked()),this,SLOT(accept()));
+    connect(bouton2,SIGNAL(clicked()),this,SLOT(reject()));
+    connect(this,SIGNAL(finished(int)),this,SLOT(dialogFinish(int)));
 }
+
+void DialogSeamCarving::dialogFinish(int exec){
+    if(exec==QDialog::Accepted){
+        int l = largeur->value();
+        int h = hauteur->value();
+        emit signalApplicationSeamCarving(l,h);
+    }
+}
+
--- Interface/dialogseamcarving.h
+++ Interface/dialogseamcarving.h
@@ -1,10 +1,21 @@
 #ifndef DIALOGSEAMCARVING_H
 #define DIALOGSEAMCARVING_H
-
-class dialogseamCarving
+#include <QtWidgets>
+class DialogSeamCarving : public QDialog
 {
+    Q_OBJECT
 public:
-    dialogseamCarving();
+    DialogSeamCarving();
+
+public slots :
+    void dialogFinish(int exec);
+
+signals :
+    void signalApplicationSeamCarving(int l,int h);
+
+private:
+   QSpinBox *largeur ;
+   QSpinBox *hauteur ;
 };
 
 #endif // DIALOGSEAMCARVING_H
